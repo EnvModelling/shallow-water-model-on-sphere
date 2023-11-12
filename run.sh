9a5a034e5678c3950d07b101487b153734221348
Paul Connolly <p.connolly@manchester.ac.uk>
2023-11-12 16:50:24 +0000

    limit processors for course

--- run.sh
+++ run.sh
@@ -8,7 +8,12 @@ if [ -z "$1" ]
 then
 	mpiexec -n 1 ./main.exe namelist.tmp
 else
-	mpiexec -n $1 ./main.exe namelist.tmp
+        if(($1 > 4)); then
+                var=4
+        else
+                var=$1
+        fi
+        mpiexec -n $var ./main.exe namelist.tmp
 fi
 rm namelist.tmp 
 
